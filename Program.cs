aa8598de72d784c8cace004366d5f41d794214a3
mohamedkrimi <mohamedkrimi703@gmail.com>
2020-10-25 16:33:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            System.Console.Write("Entrez un nombre x = ");
+            x = Double.Parse(System.Console.ReadLine());
+            if (x < 0) System.Console.WriteLine("|x| = " + (-x));
+            else System.Console.WriteLine("|x| = " + x);
+        }
+    }
+}
